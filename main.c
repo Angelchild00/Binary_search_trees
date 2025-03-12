78ea0ab355a91e41f73a2a4d5f0d7aaf2e82a538
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-03-12 09:12:57 -0400

    updated main.c for testing

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 int main (){
     TreeNode* root = NULL;
 
-    root = insertNode(root, "m");
-    root = insertNode(root, "d");
-    root = insertNode(root, "e");
-    root = insertNode(root, "r");
-    root = insertNode(root, "g");
-    root = insertNode(root, "f");
-    root = insertNode(root, "l");
-    root = insertNode(root, "k");
+    root = insertNode(root, 'm');
+    root = insertNode(root, 'd');
+    root = insertNode(root, 'e');
+    root = insertNode(root, 'r');
+    root = insertNode(root, 'g');
+    root = insertNode(root, 'f');
+    root = insertNode(root, 'l');
+    root = insertNode(root, 'k');
 
     printf("Inorder Traversal sorted: ");
     traverseTree(root);
