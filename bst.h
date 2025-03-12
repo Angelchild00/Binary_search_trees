f3268dfd4a00eb09832a28eee3e4afe4f754f2c5
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-03-12 08:31:06 -0400

    Declared Functions and Struct in bst.h

--- bst.h
+++ bst.h
@@ -1,1 +1,20 @@
+#ifndef BST_H
+#define BST_H
 #include <stdio.h>
+
+struct TreeNode
+{
+    char data;
+    struct TreeNode* pLeft;
+    struct TreeNode* pRight;
+} TreeNode;
+
+
+
+void insertNode (TreeNode** root, char node);
+TreeNode* searchNode(TreeNode* root, char targetNode);
+int countNode(TreeNode* root);
+int treeHeight(TreeNode* root);
+void traverseTree(TreeNode* root);
+
+#endif //BST_H
